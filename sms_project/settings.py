f6019f02e2d6a1f9d9b306f05bd0b1b505623eb5
Mazed CU <mazedcu@gmail.com>
2026-05-09 09:04:00 -0700

    Update droplet IP to 104.248.226.234

--- sms_project/settings.py
+++ sms_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*whwm=emn^x5sqhcof0mv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost,104.248.226.234').split(',')
 
 
 # Application definition
