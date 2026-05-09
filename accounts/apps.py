90f7606c6aaa2c9bfa075186cb0fcd0807173980
mazedcu <mazedcu@example.com>
2026-05-08 20:57:24 -0700

    Integrated Neon Postgres and restored full functionality

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,8 +4,3 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-
-    def ready(self):
-        from django.contrib.auth.models import update_last_login
-        from django.contrib.auth.signals import user_logged_in
-        user_logged_in.disconnect(update_last_login, dispatch_uid='update_last_login')
